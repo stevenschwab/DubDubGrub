4cd5eaab71ece763b92c95a8d43364b77884223e
Steven Schwab <schwabs@umich.edu>
2022-12-20 16:03:58 -0600

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Steven Schwab on 12/20/22.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
